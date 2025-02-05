b3abf6b0d1902f2844e3ac3973621ef5a6fd5074
Fraol7 <fraolmulugeta17@gmail.com>
2025-02-05 16:54:18 +0300

    constants and shadcn

---
+++ constants/index.ts
@@ -0,0 +1,16 @@
+export const devices = [
+  { title: "Smart Phone", image: "/home/device1.png" },
+  { title: "Laptop", image: "/home/device2.png" },
+  { title: "Tablet", image: "/home/device3.png" },
+  { title: "TV", image: "/home/device4.png" },
+];
+
+export const clients = [
+  { name: "Commercial Bank of Ethiopia", src: "/home/cbe.png" },
+  { name: "Ethio Telecom", src: "/home/ethiotele.png" },
+  { name: "Ethiopian Airlines", src: "/home/airlines.png" },
+  { name: "Safaricom", src: "/home/safaricom.png" },
+  { name: "Google", src: "/home/google.png" },
+  { name: "Visa & MasterCard", src: "/home/visa.png" },
+  { name: "Amazon", src: "/home/amazon.png" },
+];
