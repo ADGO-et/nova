463dd4579c516b5300d20fa556909e97e1e3970c
fraol7 <fraolmulugeta17@gmail.com>
2025-02-11 15:40:48 +0300

    12. make it the same as the design

--- components/pricing/RequestSection.tsx
+++ components/pricing/RequestSection.tsx
@@ -3,15 +3,15 @@ import React from "react";
 const RequestQuote: React.FC = () => {
   return (
     <div
-      className="flex flex-col items-center justify-center p-6 md:p-16 w-[75%] my-8 mx-auto rounded-xl"
+      className="flex flex-col items-center justify-center p-6 md:p-12 w-[75%] h-auto bg-cover bg-center my-8 mx-auto rounded-xl shadow-2xl"
       style={{ backgroundImage: "url(/pricing/bgRequest.png)" }}
     >
-      <div className="flex items-start text-white text-xl font-bold p-2 md:p-6">
+      <div className="flex items-start text-white text-xl md:text-5xl font-bold p-2 md:p-6">
         Request a Quote
       </div>
-      <div className="w-full max-w-2xl bg-foreground shadow-lg rounded-xl overflow-hidden">
-        <div className="p-4 md:p-6">
-          <form className="grid md:grid-cols-2 gap-4">
+      <div className="w-full max-w-2xl bg-foreground shadow-lg rounded-xl overflow-hidden my-12">
+        <div className="p-8 md:p-16">
+          <form className="grid md:grid-cols-2 gap-4 my-8">
             <div>
               <label className="text-background block text-sm font-medium">First Name</label>
               <input
@@ -68,7 +68,7 @@ const RequestQuote: React.FC = () => {
               ></textarea>
             </div>
             <div className="col-span-2 flex justify-end">
-              <button className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700">
+              <button className="bg-blue-600 text-white px-6 py-2 rounded-md hover:bg-blue-700">
                 Request a quote
               </button>
             </div>
