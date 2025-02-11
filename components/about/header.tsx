2c79fbca959c8e10ff3db06f12f1969d8fcf00fc
fraol7 <fraolmulugeta17@gmail.com>
2025-02-11 14:01:20 +0300

    7.make the hero images height a bit higher than the current in the about us page, services page, pricing page

--- components/about/header.tsx
+++ components/about/header.tsx
@@ -1,9 +1,9 @@
 
 const Header = () => {
   return (
-    <div className="bg-cover bg-center py-20 text-center" style={{ backgroundImage: 'url(/about_us.png)' }}>
-      <h1 className="text-4xl font-bold mb-4 text-white">Empowering TV Stations in the</h1>
-      <h1 className="text-4xl font-bold mb-4 text-white">Digital Era</h1>
+    <div className="bg-cover bg-center md:h-[385px] py-20 text-center" style={{ backgroundImage: 'url(/about_us.png)' }}>
+      <h1 className="text-4xl md:text-6xl font-bold mb-4 text-white">Empowering TV Stations in the</h1>
+      <h1 className="text-4xl md:text-6xl font-bold mb-4 text-white">Digital Era</h1>
     </div>
   );
 };
