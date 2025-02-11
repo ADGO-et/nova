2bc04e75dfc7f67554715d92eee82a53b49a0177
fraol7 <fraolmulugeta17@gmail.com>
2025-02-11 15:08:10 +0300

    10. reduce the width of the "Checkout our services that we provide" section in the services page

--- components/services/services.tsx
+++ components/services/services.tsx
@@ -1,14 +1,15 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import { FaArrowAltCircleRight } from "react-icons/fa";
 
 const Services = () => {
   return (
-    <div className="container mx-auto py-12">
+    <div className="container mx-auto w-[80%] py-12">
       <h2 className="text-3xl font-bold text-center mt-10">Check Out Our Services </h2>
       <h2 className="text-3xl font-bold text-center mb-10">That We Provide</h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
         <div className="flex flex-col items-center p-6 rounded-xl ">
-          <img src="/innovation.svg" alt="Innovation Icon 1" className="w-24 h-24 mb-4" />
+          <Image src="/innovation.svg" alt="Innovation Icon 1" width={24} height={24} className="mb-4" />
           <h3 className="text-xl font-semibold mb-2">1.	Streaming Technology</h3>
           <p className="text-center text-gray-700 mb-4">In pellentesque massa vida placerat duis.
           Cursus sit amet dictum sit amet</p>
@@ -18,7 +19,7 @@ const Services = () => {
         </Button>
         </div>
         <div className="flex flex-col items-center p-6 rounded-xl ">
-          <img src="/reliable.svg" alt="Innovation Icon 1" className="w-24 h-24 mb-4" />
+          <Image src="/reliable.svg" alt="Innovation Icon 1" width={24} height={24} className="mb-4" />
           <h3 className="text-xl font-semibold mb-2">2.	Custom App Development</h3>
           <p className="text-center text-gray-700 mb-4">In pellentesque massa vida placerat duis.
           Cursus sit amet dictum sit amet</p>
@@ -28,7 +29,7 @@ const Services = () => {
         </Button>
         </div>
         <div className="flex flex-col items-center p-6 rounded-xl ">
-          <img src="/customization.svg" alt="Innovation Icon 1" className="w-24 h-24 mb-4" />
+          <Image src="/customization.svg" alt="Innovation Icon 1" width={24} height={24} className="mb-4" />
           <h3 className="text-xl font-semibold mb-2">3.	Analytics & Insights</h3>
           <p className="text-center text-gray-700 mb-4">In pellentesque massa vida placerat duis.
           Cursus sit amet dictum sit amet</p>
