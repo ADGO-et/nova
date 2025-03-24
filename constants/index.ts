604f72c99d12116eda4b31c91e22a6d770cae92b
Melakeselam Yitbarek <Melakeselamyitbarek2012@gmail.com>
2025-03-24 19:29:57 +0300

    change

--- constants/index.ts
+++ constants/index.ts
@@ -30,7 +30,7 @@ export const testimonials = [
   },
   {
     id: 4,
-    text: "Nova revolutionized our content delivery. Their scalable platform and reliable streaming have been crucial to expanding reach and engaging our audience.Nova has revolutionized our content delivery. Their scalable platform and reliable streaming have been crucial to expanding our reach and engaging our audience effectively.",
+    text: "Nova revolutionized our content delivery. Their scalable platform and reliable streaming have been crucial to expanding reach and engaging our audience.",
   },
   {
     id: 5,
